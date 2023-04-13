1c42f4c211b7fd83b4cf89bcb608ac0c9505b275
zhongming Fan <zhongmingfan@canway.net>
2023-04-13 11:46:24 +0800

    deploy: oracle 18c exporter

--- weops/pipe-tools/exporter/template/build.sh
+++ weops/pipe-tools/exporter/template/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for version in 11g 12c 19c 21c; do
+for version in 11g 12c 18c 19c 21c; do
   # 单点
   standalone_output_file="standalone_${version}.yaml"
   sed "s/{{VERSION}}/${version}/g;" standalone.tpl > ../standalone/${standalone_output_file}
